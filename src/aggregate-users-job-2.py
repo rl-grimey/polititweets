51f912572c51ee5b122255b9e92e972711af94dc
John M. Cook <john-cook@uiowa.edu>
2016-11-17 21:36:01 -0600

    cores drove me to greed

--- src/aggregate-users-job-2.py
+++ src/aggregate-users-job-2.py
@@ -58,7 +58,7 @@ class tweetParser:
 		for filename in os.listdir(dataDir):
 			# only .csv files
 			files.append(filename)
-		files = files[241:480]
+		files = files[121:240]
 	    	#enumerate over the threads
 		#This is a dumb queue for now.
 		#we run 24 threads, wait for them all to finish, then start 24 more
