5290d696d126aa57979f1a9c3b8f2662264c9f8b
John M. Cook <john-cook@uiowa.edu>
2016-11-17 21:54:31 -0600

    fixed folder path

--- src/aggregate-users-job-3.py
+++ src/aggregate-users-job-3.py
@@ -249,7 +249,7 @@ class tweetParser:
 
 		# Get parent
 		parDir = os.path.abspath(os.path.join(fName, os.pardir))
-		parDirAgg = parDir + '-agg/'
+		parDirAgg = parDir + '-agg3/'
 		fNameAgg = self.createAggFName(fName)
 
 		combined = parDirAgg + fNameAgg
