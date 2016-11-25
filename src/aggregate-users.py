ff8fc161f06796f02ae089a4c165cc4582f18146
John M. Cook <john-cook@uiowa.edu>
2016-11-25 13:51:27 -0600

    added logging

--- src/aggregate-users.py
+++ src/aggregate-users.py
@@ -5,6 +5,7 @@ import csv
 import json
 import time
 import pdb
+import logging
 from threading import Thread, Lock, Condition
 from time import strftime, strptime
 
@@ -48,7 +49,7 @@ class tweetParser:
 		threadsActive = 0
 		dataDir = sys.argv[1]
 		if os.access(dataDir, os.F_OK):
-			print (dataDir + " dir exists!")
+			logging.logger(dataDir + " dir exists!")
 		else:
 			return ("Couldn't find your directory, sorry.")
 
@@ -73,18 +74,18 @@ class tweetParser:
 				if filename.endswith(".txt"):
 					path = (dataDir+filename)
 					threadName = "Thread-"+str(threadsActive)
-					print(threadName+" - filename: "+str(filename))
+					logging.logger(threadName+" - filename: "+str(filename))
 					thread = Thread(target = self.aggregateFile, args = (path, aggDir))
 					thread.name = threadName
  					threads.append(thread)
 					threads[threadsActive].start()
-				   	print("Starting {}".format(threads[threadsActive].name))
+				   	logging.logger("Starting {}".format(threads[threadsActive].name))
 				   	key = threads[threadsActive].name
 				   	self.asyncWriter(key, start = 'start')
 					threadsActive+=1
 					filesProcessed+=1
 					time.sleep(1)
-			print("Active Thread Pool: {} threads are started".format(threadsActive))
+			logging.logger("Active Thread Pool: {} threads are started".format(threadsActive))
 			#wait for all threads to finish
 			for thread in threads:
 				thread.join()
@@ -108,16 +109,16 @@ class tweetParser:
 	def asyncWriter(self, key, start = None):
 		while self.lock.locked():
 			self.cv.wait()
-		else:
-			self.lock.acquire()
-			self.cv.acquire()
-			if start != None:
-				self.threadDurationHistory[key] = [time.ctime(time.time())]
-			else:
-				self.threadDurationHistory[key].append(time.ctime(time.time()))
 
-			self.lock.release()
-			self.cv.notify_all()
+		self.lock.acquire()
+		self.cv.acquire()
+		if start != None:
+			self.threadDurationHistory[key] = [time.ctime(time.time())]
+		else:
+			self.threadDurationHistory[key].append(time.ctime(time.time()))
+
+		self.lock.release()
+		self.cv.notify_all()
 
 
 	def createAggFName(self, fName):
